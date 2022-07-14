11fac23657749fa590d96d3b9f7cb08518e8dd0b
MaloJaboulet <malo@cojabou.com>
2022-07-14 15:26:26 +0200

    Small bug fix

--- src/main/java/ch/zli/m223/ksh19a/mj/CRM/controller/SchuelerWebController.java
+++ src/main/java/ch/zli/m223/ksh19a/mj/CRM/controller/SchuelerWebController.java
@@ -1,6 +1,7 @@
 package ch.zli.m223.ksh19a.mj.CRM.controller;
 
 import ch.zli.m223.ksh19a.mj.CRM.controller.DTO.SchuelerDTO;
+import ch.zli.m223.ksh19a.mj.CRM.model.Klasse;
 import ch.zli.m223.ksh19a.mj.CRM.model.Schueler;
 import ch.zli.m223.ksh19a.mj.CRM.service.KlassenService;
 import ch.zli.m223.ksh19a.mj.CRM.service.SchuelerService;
@@ -8,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -18,6 +20,8 @@ public class SchuelerWebController {
 
     @Autowired
     private SchuelerService schuelerService;
+    @Autowired
+    private KlassenService klassenService;
 
     @GetMapping("/schueler")
     String getSchuelerList(Model model) {
@@ -39,15 +43,30 @@ public class SchuelerWebController {
     public String insertSchueler(Model model,
                                  @RequestParam("vorname") String vorname,
                                  @RequestParam("nachname") String nachname,
-                                 @RequestParam("klasse") String klasse,
+                                 @RequestParam("klasse") String klassenName,
                                  @RequestParam("password") String password) {
-        Schueler schueler = schuelerService.insertSchueler(vorname, nachname, klasse, password);
-       model.addAttribute("schuelerList", schuelerService.getAllSchueler());
-       return "schueler";
+
+        if (klassenService.getAllKlassen().stream()
+                .anyMatch((Klasse klasse) -> {
+                    return klasse.getName().equals(klassenName);
+                })) {
+            Schueler schueler = schuelerService.insertSchueler(vorname, nachname, klassenName, password);
+        }
+
+        model.addAttribute("schuelerList", schuelerService.getAllSchueler());
+        return "redirect:/schueler";
+    }
+
+    @GetMapping("/schuelerDelete/{vorname}/{nachname}")
+    public String deleteSchueler(Model model,
+                                 @PathVariable("vorname") String vorname, @PathVariable("nachname") String nachname) {
+        schuelerService.deleteSchueler(vorname, nachname);
+        model.addAttribute("schuelerList", schuelerService.getAllSchueler());
+        return "redirect:/schueler";
     }
 
 
-        @GetMapping("/admin")
+    @GetMapping("/admin")
     String getToAdminPage() {
         return "admin_page";
     }
